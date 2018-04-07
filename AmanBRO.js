26a1e93f84ef59f0b451c7c45ab9ed705e438fe2
XCDNX <38094734+xcdnx@users.noreply.github.com>
2018-04-07 21:23:11 +0700

    Update AmanBRO.js

--- AmanBRO.js
+++ AmanBRO.js
@@ -26,7 +26,7 @@ if(x=="2"){xxx="mesothelioma-compensation-acquiring.html"}
 if(x=="3"){xxx="claiming-compensation-for-mesothelioma.html"}
 if(x=="4"){xxx="mesothelioma-benign-or-malignant.html"}
 if(x=="5"){xxx="understand-your-cancer-diagnosis.html"}
-if(x=="6"){xxx="mesothelioma-compensation-financial.html"}a_to_ve[i].href="https://kesafelinks.blogspot.com/2018/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="6"){xxx="mesothelioma-compensation-financial.html"}a_to_ve[i].href="https://www.lewatsini.ga/2018/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
